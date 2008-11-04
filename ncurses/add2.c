47be577612764a239ed5385b750b245f5ea4ef3f
Luis Armendariz <luis.armendariz@gmail.com>
2008-11-04 03:11:16 -0800

    ncurses examples

---
+++ ncurses/add2.c
@@ -0,0 +1,16 @@
+#include <ncurses.h>
+
+int main(void)
+{
+    char text1[] = "Oh give me a clone!\n";
+    char text2[] = "Yes a clone of my own!";
+
+    initscr();
+    addstr(text1);  /* add the first string */
+    addstr(text2);  /* add the second stirng */
+    refresh();      /* display the result */
+    getch();        /* wait */
+
+    endwin();
+    return 0;
+}
